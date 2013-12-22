a74d34d671c5f29c90255175e35691bbf9760c38
Eko Suprapto Wibowo <swdev.bali@gmail.com>
2013-12-22 21:32:48 +0700

    initial

---
+++ MatchMePy/hello.cpp
@@ -0,0 +1,13 @@
+#include <boost/python/module.hpp>
+#include <boost/python/def.hpp>
+
+char const* greet()
+{
+   return "hello, world";
+}
+
+BOOST_PYTHON_MODULE(hello_ext)
+{
+    using namespace boost::python;
+    def("greet", greet);
+}
