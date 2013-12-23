d8383534bf44e8ba9141010fa4f7b28b59d2175e
Eko Suprapto Wibowo <swdev.bali@gmail.com>
2013-12-23 13:56:33 +0700

    yep, iso map

--- Classes/HelloWorldScene.h
+++ Classes/HelloWorldScene.h
@@ -2,9 +2,13 @@
 #define __HELLOWORLD_SCENE_H__
 
 #include "cocos2d.h"
+using namespace cocos2d;
 
 class HelloWorld : public cocos2d::Layer
 {
+private:
+	CCTMXTiledMap *tileMap;
+	CCTMXLayer *background;
 public:
     // there's no 'id' in cpp, so we recommend returning the class instance pointer
     static cocos2d::Scene* createScene();
@@ -12,8 +16,7 @@ public:
     // Here's a difference. Method 'init' in cocos2d-x returns bool, instead of returning 'id' in cocos2d-iphone
     virtual bool init();  
     
-    // a selector callback
-    void menuCloseCallback(Object* pSender);
+   
     
     // implement the "static create()" method manually
     CREATE_FUNC(HelloWorld);
